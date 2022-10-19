dbfe67718bff7e64ff8979203911e6d479726944
usrtax <i@user.tax>
2022-10-19 21:55:03 +0800

    ♨

--- uint.sql
+++ uint.sql
@@ -206,23 +206,3 @@ CREATE FUNCTION int1um (int1)
 CREATE OPERATOR - (
   PROCEDURE = int1um, RIGHTARG = int1
 );
-
-UPDATE
-  pg_type
-SET oid = 990001
-WHERE typname = 'u8';
-
-UPDATE
-  pg_type
-SET oid = 990002
-WHERE typname = 'u16';
-
-UPDATE
-  pg_type
-SET oid = 990003
-WHERE typname = 'u32';
-
-UPDATE
-  pg_type
-SET oid = 990004
-WHERE typname = 'u64';
